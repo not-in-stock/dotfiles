18249fdaeb1a28e8aa79e0e71399c33e30279991
Andrey Fedorov <oran9e.red@gmail.com>
2024-02-20 03:35:55 +0100

    Remove evil-lion (aligns text) package

--- spacemacs.el
+++ spacemacs.el
@@ -151,7 +151,8 @@ This function should only modify configuration layer settings."
    ;; A list of packages that will not be installed and loaded.
    dotspacemacs-excluded-packages '(evil-nerd-commenter
                                     golden-ratio
-                                    treemacs-icons-dired)
+                                    treemacs-icons-dired
+                                    evil-lion)
 
    ;; Defines the behaviour of Spacemacs when installing packages.
    ;; Possible values are `used-only', `used-but-keep-unused' and `all'.
