cb0db1288db6ad671051ac66cec315cf71501003
Andrey Fedorov <oran9e.red@gmail.com>
2025-05-06 12:10:42 +0200

    Reformat code

--- spacemacs.el
+++ spacemacs.el
@@ -847,12 +847,13 @@ before packages are loaded."
   (require 'treemacs)
   (require 'treemacs-themes)
 
-  (treemacs-modify-theme "Default"
-    :icon-directory "/other/icons/dir"
-    :config
-    (progn
-      (treemacs-create-icon :icon "▸ " :extensions (dir-closed) :fallback "▸ ")
-      (treemacs-create-icon :icon "▾ " :extensions (dir-open) :fallback "▾ ")))
+  (treemacs-modify-theme
+   "Default"
+   :icon-directory "/other/icons/dir"
+   :config
+   (progn
+     (treemacs-create-icon :icon "▸ " :extensions (dir-closed) :fallback "▸ ")
+     (treemacs-create-icon :icon "▾ " :extensions (dir-open) :fallback "▾ ")))
 
   (treemacs-hide-gitignored-files-mode t)
 
